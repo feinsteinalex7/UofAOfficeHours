6b373e5ad7f9cc42b604fd3549516262dc0cc95f
Alex Feinstein <feinsteinalex7@gmail.com>
2020-01-17 22:14:39 -0700

    started mongo, you're up aaron

--- src/app.js
+++ src/app.js
@@ -1,148 +1,86 @@
 const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
 const ObjectID = mongodb.ObjectID;
+const MongoClient = mongodb.MongoClient;
 
-const connectionURL = 'mongodb://127.0.0.1:27017';
-const databaseName = 'UofA-Hours';
-
-MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
-    if (error) {
-        return console.log("Unable to connect to database.");
+class BackEndMain {
+    constructor() {
+        this.databaseConnection = new DatabaseConnection();
+        databaseConnection.init();
     }
-    
-    const db = client.db(databaseName);
+}
 
-    /*
-    db.collection('users').insertOne({
-       name: 'Alex',
-       age: 20 
-    }, (error, result) => {
-        if (error) {
-            return console.log("Unable to insert user");
+
+class DatabaseConnection {
+    constructor() {
+        this.mongo = {
+            db: null,
+            connectionURL: 'mongodb://127.0.0.1:27017',
+            databaseName: 'UofA-Hours',
         }
+    }
 
-        console.log(result.ops);
-    });
-    
-    const updatePromise = db.collection("users").updateOne({
-        _id: new ObjectID("5e1ba82a3f49ee0f31f3376d")
-    }, {
-        $inc: {
-            age: 1
-        }
-    });
-
-    updatePromise.then((result) => {
-        console.log(result);
-    }).catch((error) => {
-        console.log(error);
-    });
-
-    db.collection("tasks").updateMany({
-        completed: false
-    }, {
-        $set: {
-            completed: true
-        }
-    }).then((result) => {
-        console.log(result);
-    }).catch((error) => {
-        console.log(error);
-    });
-    */
-    
-    /*
-    db.collection("users").deleteMany({
-        age: 20
-    }).then((result) => {
-        console.log(result);
-    }).catch((error) => {
-        console.log(error);
-    }); 
-    */
-
-    db.collection("tasks").deleteOne({
-        _id: new ObjectID("5e1bcdedcfe951119fe6be7a")
-    }).then((result) => {
-        console.log(result);
-    }).catch((error) => {
-        console.log(error);
-    }); 
+    init() {
+        // Initialize Mongo
+        MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
+            if (error) {
+                return console.log("Unable to connect to database.");
+            }
+            
+            this.db = client.db(databaseName);
+        });
+    }    
 
 
-    /*
-    db.collection('users').insertMany([
-        {
-            name: 'Jen',
-            age: 28
-        },
-        {
-            name: 'Gunther',
-            age: 27
-        }
-    ], (error, result) => {
-        if (error) {
-            return console.log("Unable to insert documents!");
-        }
-        console.log(result.ops);
-    });
-    */
+    // Getter methods
+    getProfessor(professor) {
+        let professor_document;
+        db.collection('office_hours').findOne({
+            "professor": professor
+        }).then((result) => {
+            // Add the class
+            professor_document = result;
+        }).catch((error) => {
+            console.log("Error: " + error);
+        });
+        return professor_document;
+    }
 
-    /*
-    db.collection('tasks').insertMany([
-        {
-            name: "task 1",
-            description: "the easiest task",
-            completed: true
-        },
-        {
-            name: "task 2",
-            description: "the medium task",
-            completed: false
-        },
-        {
-            name: "task 3",
-            description: "the hardest task",
-            completed: true
-        }
-    ], (error, result) => {
-        if (error) {
-            return console.log("Unable to insert these documents!");
-        }
-        console.log(result.ops);
-    });
-    */
 
-    /*
-    db.collection('users').findOne({name: 'Jen'}, (error, user) => {
-        if (error) {
-            return console.log("Unable to fetch.");
-        }
-        console.log(user);
-    });
+    // Insert methods
+    insertNewProfessor(professor) {
+        db.collection('office_hours').insertOne({
+            "professor": professor,
+            "classes" : {}
+         }).then((result) => {
+            console.log("Success!");
+         }).catch((error) => {
+            console.log("Error: " + error);
+         });
+    }
 
-    db.collection('users').find({age: 27}).toArray((error, users) => {
-        console.log(users);
-    });
-    */
+    insertNewClass(professor, class_name) {
+        // Get professor's document
+        let professor_document = this.getProfessor(professor);
+        professor_document
 
-    /*
-    db.collection('tasks').findOne({_id: new ObjectID("5e1bcdedcfe951119fe6be78")}, (error, doc) => {
-        if (error) {
-            return console.log("findOne error!");
-        }
-        console.log("findOne", doc);
-    });
+        
+        db.collection('office_hours').insertOne({
+            "professor": professor,
+            "classes" : {}
+         }).then((result) => {
+            console.log("Success!");
+         }).catch((error) => {
+            console.log("Error: " + error);
+         });
+    }
 
-    db.collection('tasks').find({completed: true}).toArray((error, docs) => {
-        if (error) {
-            return console.log("find error!");
-        }
-        console.log("find", docs);
-    });
-    */
+    insertNewOfficeHour(professor, class_name, startTime, endTime) {
+
+    }
+}
 
 
 
-});
+
+
 
