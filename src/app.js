4242a1dabb449f1957ed14ca47d9e0fcddd12093
aaronfrank123 <aaron.frank123@gmail.com>
2020-01-17 23:50:32 -0700

    backend start

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ const MongoClient = mongodb.MongoClient;
 class BackEndMain {
     constructor() {
         this.databaseConnection = new DatabaseConnection();
-        databaseConnection.init();
+        console.log("db made");
+        this.databaseConnection.init().then((result) => {
+            this.databaseConnection.insertNewProfessor("test").then((result) => {
+                this.databaseConnection.getProfessor("test").then((result2) => {
+                    console.log(result2);
+                });
+            });
+            this.databaseConnection.insertNewClass("lol");
+            console.log(this.databaseConnection.getProfessor("test"));
+        });
     }
 }
 
@@ -21,41 +30,56 @@ class DatabaseConnection {
 
     init() {
         // Initialize Mongo
-        MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
-            if (error) {
-                return console.log("Unable to connect to database.");
-            }
-            
-            this.db = client.db(databaseName);
+        return new Promise((resolve, reject) => {
+            MongoClient.connect(this.mongo.connectionURL, {useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
+                if (error) {
+                    reject(error);
+                    console.log("Unable to connect to database.");
+                } else {
+                    this.mongo.db = client.db(this.mongo.databaseName);
+                    resolve(this.mongo.db);
+                }
+            });
         });
     }    
 
 
     // Getter methods
     getProfessor(professor) {
-        let professor_document;
-        db.collection('office_hours').findOne({
-            "professor": professor
-        }).then((result) => {
-            // Add the class
-            professor_document = result;
-        }).catch((error) => {
-            console.log("Error: " + error);
+        let professor_document = null;
+        return new Promise((resolve, reject) => {
+            this.mongo.db.collection('office_hours').findOne({
+                "professor": professor
+            }).then((res) => {
+                // Add the class
+                if (res == null) {
+                    professor_document = "error";
+                }
+                professor_document = res;
+                resolve(professor_document);
+            }).catch((error) => {
+                professor_document = "error";
+                reject(error);
+                console.log("Error: " + error);
+            });
         });
-        return professor_document;
     }
 
 
     // Insert methods
     insertNewProfessor(professor) {
-        db.collection('office_hours').insertOne({
-            "professor": professor,
-            "classes" : {}
-         }).then((result) => {
-            console.log("Success!");
-         }).catch((error) => {
-            console.log("Error: " + error);
-         });
+        return new Promise((resolve, reject) => {
+            this.mongo.db.collection('office_hours').insertOne({
+                "professor": professor,
+                "classes" : {}
+            }).then((result) => {
+                resolve();
+                console.log("Success!");
+            }).catch((error) => {
+                reject(error);
+                console.log("Error: " + error);
+            });
+        });
     }
 
     insertNewClass(professor, class_name) {
@@ -64,7 +88,7 @@ class DatabaseConnection {
         professor_document
 
         
-        db.collection('office_hours').insertOne({
+        this.mongo.db.collection('office_hours').insertOne({
             "professor": professor,
             "classes" : {}
          }).then((result) => {
@@ -79,8 +103,4 @@ class DatabaseConnection {
     }
 }
 
-
-
-
-
-
+let backend = new BackEndMain();
